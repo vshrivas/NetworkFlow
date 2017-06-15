7d1aef11ecfec3214ba03514012f4331d86cb42d
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2017-06-14 18:03:41 -0700

    Add methods to DummyNode
    Add more functionality to DummyNode.

--- storage/DummyNode.py
+++ storage/DummyNode.py
@@ -1,13 +1,9 @@
 class DummyNode:
-    
+
     def __init__(self):
         self.relationships = []
         self.properties = []
         self.labels = []
-
-    # This method adds a node with a relationship to this node's adj list
-    def addRelationship(self, rel):
-        self.relationships.append(rel)
 
     # This method adds property data to a node
     def addProperty(self, prop):
@@ -16,3 +12,6 @@ class DummyNode:
     # This method adds labels to a node
     def addLabel(self, nodeLabel):
         self.labels.append(nodeLabel)
+
+    def getLabels(self):
+        return self.labels
