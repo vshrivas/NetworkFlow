51838d68394518d92f36f36f2f1c03d8ed51d5e4
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2017-06-22 11:10:05 -0700

    Delete unnecessary files

--- storage/NodePage.py
+++
@@ -1,8 +0,0 @@
-class NodePage:
-	# node pages will be 1000 bytes
-	PAGE_SIZE = 1000
-
-	# NodePages will be instantiated through NodeFiles
-	# which will pass in the pageNum
-	def __init__(self, pageNum):
-		self.pageNum = pageNum
