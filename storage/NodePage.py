effdc76b2653ec386ceb1d543f8f22934b737929
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-01-25 21:00:12 -0800

    Add readNode method to NodePage
    readNode method will get the IDs of the first relationship, property,
    and label and get the lists of relationships, properties, and labels
    for the node.

--- storage/NodePage.py
+++ storage/NodePage.py
@@ -1,1 +1,13 @@
 class NodePage(DataPage):
+	ENTRY_SIZE = Node.storageSize
+
+	def __init__(self, pageID, datafile):
+		super().__init__(pageID, datafile)
+
+	def readNode(self, nodeID):
+		# returns IDs of first relationship, property, and label
+		# get list of relationships
+			# will need to get properties for each relationship
+			
+		# get list of properties
+		# get list of labels
