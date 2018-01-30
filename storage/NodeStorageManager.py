7537aa8072d50788e21571d864a3553403dddc33
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-01-30 01:19:15 -0800

    Move readNode to NodeStorageManager
    NodeStorageManager should handle reading nodes.

--- storage/NodeStorageManager.py
+++ storage/NodeStorageManager.py
@@ -1,1 +1,28 @@
+# has metadata keeping track of number of node files
 class NodeStorageManager(StorageManager):
+	nodeFiles = []
+	metaDataPath = "datastore"
+	def __init__(self):
+		# open node storage meta data file
+		# read number of node files 
+		# create file objects for each of the node files, and make a list of these
+
+	readNode(pageID, nodeID):
+		# file number of node file page is in
+		fileNo = pageID / DataFile.MAX_FILE_SIZE 
+		nodeFile = nodeFiles[fileNo - 1]
+
+		# returns a list of first relationship, first property, and first label associated with node
+		nodeAttributes = nodeFile.readNode(pageID, nodeID)
+
+		firstRelID = nodeAttributes[0]
+		firstPropID = nodeAttributes[1]
+		firstLabelID = nodeAttributes[2]
+
+		nodeRelationships = RelationshipStoreManager.getRelChain(firstRelID)
+		nodeProperties = PropertyStoreManager.getPropChain(firstPropID)
+		nodeLabels = LabelStoreManager.getLabelChain(firstLabelID)
+
+	createNode():
+
+	writeNode(pageID, nodeID):
