a32300bd44f44161f07926fc43917d8d4e29ba0c
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-02-25 22:48:07 -0800

    Add writeLabels method to label storage manager
    writeLabels takes in a list of labels and write them to the appropriate
    file.

--- storage/LabelStorageManager.py
+++ storage/LabelStorageManager.py
@@ -1,1 +1,19 @@
 class LabelStorageManager(StorageManager):
+	def writeLabels(labels):
+        # write labels to label file
+        for labelIndex in range(0, len(labels)):
+            label = labels[labelIndex]
+            if DEBUG:
+                print("writing {0} label ".format(label.getLabelID()))
+
+            # case of last label
+            if labelIndex == len(labels) - 1:
+                if DEBUG:
+                    print("no next label")
+                # set next label's id to -1
+                nextLabelID = -1
+            # case of any other label
+            else:
+                nextLabelID = (labels[labelIndex + 1]).getLabelID()
+            # write label
+            label.writeLabel(nextLabelID) 
