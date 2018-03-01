656a233a5b5afa82907b62ea745dd36ec310f943
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-02-28 21:16:32 -0800

    Update writeNode and createNode
    Update write methods in node storage manager to use other data storage
    managers to write lists of data objects for node. Update create method
    to check if last node file has space, else make new file.

--- storage/NodeStorageManager.py
+++ storage/NodeStorageManager.py
@@ -7,29 +7,74 @@ class NodeStorageManager(StorageManager):
 		# read number of node files 
 		# create file objects for each of the node files, and make a list of these
 
-	readNode(pageID, nodeID):
+	readNode(pageID):
+		pageID = nodeID[0] 			# pageID[0] = 0, pageID[1] = pageIndex
+		nodeIndex = nodeID[1]
+
+		pageIndex = pageID[1]		# which page node is in, page IDs are unique across all files
+
 		# file number of node file page is in
-		fileNo = pageID / DataFile.MAX_FILE_SIZE 
-		nodeFile = nodeFiles[fileNo - 1]
+		fileNo = pageIndex / DataFile.MAX_FILE_PAGES 
+		nodeFile = nodeFiles[fileNo]
 
 		# returns a list of first relationship, first property, and first label associated with node
-		nodeAttributes = nodeFile.readNode(pageID, nodeID)
+		nodeID = [pageID, nodeIndex]
+		nodeAttributes = nodeFile.readNode(pageID, nodeIndex)
 
 		firstRelID = nodeAttributes[0]
 		firstPropID = nodeAttributes[1]
 		firstLabelID = nodeAttributes[2]
 
-		nodeRelationships = RelationshipStoreManager.getRelChain(firstRelID)
-		nodeProperties = PropertyStoreManager.getPropChain(firstPropID)
-		nodeLabels = LabelStoreManager.getLabelChain(firstLabelID)
+	# takes in a node object 
+	def writeNode(node):
+        """Writes this node to the node's node file according to the storage 
+        format given in the class description and writes this node's relationships, 
+        properties, and labels to the node's relationship file, property file, and 
+        label file, respectively.
+        """
+        if DEBUG:
+            print("properties in node")
+            for prop in node.properties:
+                print(prop.getID())
 
-		node = Node(pageID, nodeID)
-		node.addRelationships(nodeRelationships)
-		node.addProperties(nodeProperties)
-		node.addLabels(nodeLabels)
+            print("labels in node:")
+            for label in node.labels:
+                print(label.getLabelID())
 
-		return node
+            print("writing node...")
 
-	createNode():
+        nodeID = node.nodeID
+        pageID = nodeID[0] 			# pageID[0] = 0, pageID[1] = pageIndex
+		nodeIndex = nodeID[1]
 
-	writeNode(pageID, nodeID):
+		pageIndex = pageID[1]		# which page node is in, page IDs are unique across all files
+		# file number of node file page is in
+		fileNo = pageIndex / DataFile.MAX_FILE_PAGES 
+		nodeFile = nodeFiles[fileNo - 1]
+
+        nodeFile.writeNode(node)
+
+        if DEBUG:
+            print("writing relationships to relationship file ...")
+
+        RelationshipStoreManager.writeRelationships(node.relationships)
+        
+        if DEBUG:
+            print("writing properties to property file ...")
+
+        PropertyStoreManager.writeProperties(node.properties)
+
+        if DEBUG:
+            print("writing labels to property file ...")
+
+        LabelStoreManager.writeLabels(node.labels)
+
+	def createNode():
+		# find if last file's last page has space
+		lastFile = nodeFiles[len(nodeFiles) - 1]
+		nodePage = lastFile.hasSpace()
+
+		# last node file is full
+		if nodePage == None:
+			# make new file
+			newFile = NodeFile(len(nodeFiles))
