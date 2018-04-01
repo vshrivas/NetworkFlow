8cc505f0601526c68bf7ff031153ca8a8733f9f9
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-04-01 08:04:30 -0700

    Modify lock manager to break write deadlock
    Modify lock manager to detect write lock deadlock. Add lock to
    pageOwners list to fix list corruption.

--- storage/LockManager.py
+++ storage/LockManager.py
@@ -3,9 +3,10 @@ from RelationshipPage import RelationshipPage
 import random
 import threading
 
-class LockManager(Object):
+class LockManager:
 	# dictionary of pageID and owner 
 	pageOwners = {}
+	pageOwnersLock = threading.Lock()
 
 	# checks if there is a potential for deadlock
 	# returns false if there isn't deadlock
@@ -18,26 +19,57 @@ class LockManager(Object):
 		# if t1 -> t2 -> t3 ... t1, where -> takes us to the thread holding the lock we need, then
 		# letting t1 wait on the lock will cause deadlock in the system
 
-		nextThread = LockManager.pageOwners[tuple(userThread.waiting.pageID)]
+		if userThread.waiting is None:
+			return False
+
+		pageOwnerKey = tuple(userThread.waiting.pageID)
+
+		# page has an owner
+		if pageOwnerKey in LockManager.pageOwners.keys():
+			nextThread = LockManager.pageOwners[tuple(userThread.waiting.pageID)]
+			if nextThread.waiting is None:
+				return False
+		# page has no owner
+		else:
+			return False
 
 		# while nextThread isn't none (chain ends)
 		# or chain circles back to userThread
 		# there is no other end possibility, since if we circled back to some other thread, deadlock
 		# would have existed in the system 
-		while(nextThread != None or nextThread != userThread) {
-			# nextThread is the thread we 
-			nextThread = LockManager.pageOwners[tuple(nextThread.waiting.pageID)]
-		}
+		while(nextThread is not None and nextThread.name != userThread.name):
+			pageOwnerKey = tuple(nextThread.waiting.pageID)
 
-		if nextThread == None:
+			# nextThread is the thread we 
+			if nextThread.waiting is None:
+				nextThread = None
+			elif pageOwnerKey in LockManager.pageOwners.keys():
+				nextThread = LockManager.pageOwners[tuple(nextThread.waiting.pageID)]
+			else:
+				nextThread = None
+
+		if nextThread is None:
+			print('No deadlock detected')
 			return False
 
+		elif nextThread.name == userThread.name:
+			print('Deadlock condition detected!')
+			userThread.waiting = None
+			return True
+
 		else:
+			print('next thread name: {0}'.format(nextThread.name))
+			print('user thread name: {0}'.format(userThread.name))
+			print('Error: should never reach this!')
 			return True
 
 
 	def makePageOwner(thread, page):
+		LockManager.pageOwnersLock.acquire()
 		LockManager.pageOwners[tuple(page.pageID)] = thread
+		LockManager.pageOwnersLock.release()
 
 	def removePageOwner(page):
+		LockManager.pageOwnersLock.acquire()
 		del LockManager.pageOwners[tuple(page.pageID)]
+		LockManager.pageOwnersLock.release()
