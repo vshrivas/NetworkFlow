0e68d82748f6aedbd15c09226497179296b858e4
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2018-04-01 08:55:48 -0700

    Implement Read Write Lock
    Implements read-preferring read/write lock.

---
+++ storage/RWLock.py
@@ -0,0 +1,33 @@
+import threading
+
+# implements read-preferring read/write lock
+
+class RWLock:
+	def __init__(self):
+		self.lockReadersProtect = threading.lock()
+		self.lockWritersExclude = threading.lock()
+		self.numReaders = 0
+
+	def acquire_read(self):
+		self.lockReadersProtect.acquire()
+
+		self.numReaders += 1
+		if self.numReaders == 1:
+			self.lockWritersExclude.acquire()
+
+		self.lockReadersProtect.release()
+
+	def release_read(self):
+		self.lockReadersProtect.acquire()
+
+		self.numReaders -= 1
+		if self.numReaders == 0:
+			self.lockWritersExclude.release()
+
+		self.lockReadersProtect.release()
+
+	def acquire_write(self):
+		self.lockWritersExclude.acquire()
+
+	def release_write(self):
+		self.lockWritersExclude.release()
